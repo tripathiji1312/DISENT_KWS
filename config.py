16af6ac6ab0f52e7f6858781bff5c2d945440f09
Swarnim Tripathi <swarnim.tr@gmail.com>
2026-06-06 23:48:01 +0530

    midified batch sixe

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ ECAPA_SE_RATIO = 4
 # ---------------------------------------------------------------------------
 # Training
 # ---------------------------------------------------------------------------
-BATCH_SIZE             = 128
+BATCH_SIZE             = 64
 NUM_SPEAKERS_VOXCELEB  = 1251   # VoxCeleb1 only (1,251 speakers)
 NUM_KEYWORDS_GSC       = 35     # Google Speech Commands v2
 
